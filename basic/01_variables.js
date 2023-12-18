e6b2b561de88741a1d1bced444a206bdf3c73d2d
PratCloud <152966350+PratCloud@users.noreply.github.com>
2023-12-18 09:49:12 +0000

    Added files

---
+++ basic/01_variables.js
@@ -0,0 +1,18 @@
+const AccountId = 122445
+let AccountEmail = "keshav@google.com"
+var AccountPassword = "12345"
+AccountCity = "Nagpur"
+
+//console.log(AccountId);
+
+//console.table([AccountEmail,AccountId,AccountPassword,AccountCity]);
+
+AccountEmail = "Pratham@gmail.com"
+AccountPassword = "123456"
+AccountCity = "Deheradun"
+
+console.table([AccountEmail,AccountPassword,AccountCity]);
+
+/*Prefer not to use var because of issue of Block scope and functional scope */
+
+
