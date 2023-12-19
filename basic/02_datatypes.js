d3be64d992997328555822bdf8d24d9115123121
PratCloud <152966350+PratCloud@users.noreply.github.com>
2023-12-19 09:51:31 +0000

    datatypes

--- basic/02_datatypes.js
+++ basic/02_datatypes.js
@@ -1,1 +1,25 @@
 console.log("chal raha hai");
+"use strict" // help us use newer verision of javascript
+
+//alert(3+3) we cannot us it because we are using node.js not browser .
+
+let name = "hitesh"
+let age = 8
+let isLoggedIn = false
+
+/*
+number = 2 to power 53
+bigint 
+string = "sfljs"
+boolean = true
+null = standalone value
+undefined = 
+symbol = unique
+
+object
+*/
+
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
