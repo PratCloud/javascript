7d2d3d8f711ebf8f560b42cca335d04307d8c7b5
PratCloud <152966350+PratCloud@users.noreply.github.com>
2023-12-19 10:52:05 +0000

    conversion

---
+++ basic/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let username = "3node basic/03_conversionOperations.jsAAm"
+
+let vaulueInUsername = Number(username)
+
+console.log(typeof vaulueInUsername);
+console.log(vaulueInUsername);
+
+/******************************************** operations ********************************************/
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+*/
+
+console.log(1 + "2");
+console.log("1"+2);
+console.log("1" + 2 + 2)
+console.log(1+2+"2")
+
+
