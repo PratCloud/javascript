60a435b8cefb9d2f356e95c480f1b5fa58f884bf
PratCloud <152966350+PratCloud@users.noreply.github.com>
2023-12-22 10:52:26 +0000

    New file

---
+++ basic/05_Strings.js
@@ -0,0 +1,11 @@
+const name = "Pratham";
+const repoCount = 50;
+
+console.log(name + repoCount + "Value")
+
+console.log(`Hello my name is ${name} and currently my repo count is ${repoCount}`);
+
+const gameName = new String('wartox')
+
+console.log(gameName(2));
+
