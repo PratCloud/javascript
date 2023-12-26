fa0633a8b584b2f1f17a15f733de315f90156642
PratCloud <152966350+PratCloud@users.noreply.github.com>
2023-12-26 20:24:15 +0000

    New file Num

---
+++ basic/06_nums_And_Math.js
@@ -0,0 +1,25 @@
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.899
+
+console.log(otherNumber.toPrecision(7));
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en'));
+
+// ___________ Maths____________ //
+
+const num = -100;
+
+console.log(Math.abs(num));
+
+console.log(Math.floor(Math.random()*10));
+
+// if we want to avoid 0 in result we can add one in the above formula to get desired result.
